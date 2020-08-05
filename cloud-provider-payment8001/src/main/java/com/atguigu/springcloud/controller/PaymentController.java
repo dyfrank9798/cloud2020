b201b4a580a476df527d66fad68be6216206cab8
dyfrank9798 <936589714@qq.com>
2020-08-05 14:31:35 +0800

    11111

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,59 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.PaymentEntity;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zhangrongchuan
+ * @data 2020-08-2020/8/5-11:13
+ * @Description
+ */
+@Controller
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    /**
+     *
+     * @PostMapping用于将HTTP post请求映射到特定处理程序的方法注解
+     *
+     * 具体来说，@PosMapping是一个组合注解，是@RequestMapping(method=RequestMethod.POST)的缩写
+     *
+     *请求资源应该使用GET
+     *
+     * 添加资源应该使用POST
+     *
+     * 更新资源应该使用PUT
+     *
+     * 删除资源应该使用DELETE
+     */
+    @PostMapping(value="/payment/create")
+    public CommonResult create(PaymentEntity paymentEntity){
+        int result = paymentService.create(paymentEntity);
+        System.out.println("************插入结果"+result);
+        if(result>0){
+            return new CommonResult(200,"插入数据库成功",result);
+        }else{
+            return new CommonResult(444,"插入数据库失败！",null);
+        }
+    }
+
+    @GetMapping(value="/payment/get/{id}")
+    public CommonResult getPaymentByid(@PathVariable("id") Long id){
+        PaymentEntity paymentEntity = paymentService.getPaymentById(id);
+
+        System.out.println(paymentEntity);
+        if(paymentEntity != null){
+            return new CommonResult(200,"查询成功",paymentEntity);
+        }else{
+            return new CommonResult(200,id+"查询失败",null);
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entities.PaymentEntity;
+import com.atguigu.springcloud.service.PaymentService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zhangrongchuan
+ * @data 2020-08-2020/8/5-10:59
+ * @Description
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    //比Autowire --spring resouce是java的
+    private PaymentDao paymentDao;
+
+    public int create(PaymentEntity payment){
+        return paymentDao.create(payment);
+    };
+
+    public PaymentEntity getPaymentById(@Param("id") long id){
+        return paymentDao.getPaymentById(id);
+    };
+}
